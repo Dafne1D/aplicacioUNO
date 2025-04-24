83b908b07564e7053cd97539fc02e3cfd6d61908
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-24 13:07:57 +0200

    Create: Classes generals vuides

--- uno/logica/OrdreJugador.java
+++ uno/logica/OrdreJugador.java
@@ -1,5 +1,22 @@
 package uno.logica;
 
+import uno.interficie.UI;
+
 public class OrdreJugador {
+    public void passarTorn(){
+
+    }
+
+    public void getJugadorActiu(){
+
+    }
+
+    public void crearJugador(String nom){
+
+    }
+
+    public void barrajarOrdre(){
+
+    }
 
 }

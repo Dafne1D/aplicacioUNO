bcda0d84425eb4e21ea237aa67aabd31d909f9a6
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-24 13:03:12 +0200

    Create: Classes generals vuides

--- uno/logica/Partida.java
+++ uno/logica/Partida.java
@@ -1,5 +1,20 @@
 package uno.logica;
 
 public class Partida {
+    public void Jugar(){
 
+    }
+
+    private void preparar(){
+
+    }
+
+    private boolean torn(){
+        //si un jugador guanya es true si perd false
+        return false;
+    }
+
+    private void repartirCartes(){
+
+    }
 }

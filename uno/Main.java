539703d4285f05460798bcdac789077250a17fd4
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-24 13:09:22 +0200

    Update: Reorganitzar i afegir l'introducció de jugadors

--- uno/Main.java
+++ uno/Main.java
@@ -8,36 +8,32 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        //JUGADORS----------------------------
         ArrayList<Jugador> llistaJugador = new ArrayList<Jugador>();
 
-        Jugador j1 = new Jugador("Jugador 1");
-        llistaJugador.add(j1);
-        Jugador j2 = new Jugador ("Jugador 2");
-        llistaJugador.add(j2);
+        UI.introduirJugadors();
 
+        //MAZO-----------------------------
         Mazo mazo1 = new Mazo();
         mazo1.barrejar();
+        System.out.println("MAZO");
+        UI.mostrarCartes(mazo1.getCartes());
+
+        //PILO--------------------------------
         Pilo pilo1 = new Pilo();
         pilo1.getCartes();
-        for (int i = 0; i<38; i++){
+        for (int i = 0; i<1; i++){
             pilo1.getCartes().push(mazo1.agafarCarta());
         }
         System.out.println("PILO");
-
         UI.mostrarCartes(new ArrayList<>(pilo1.getCartes()));
-        System.out.println("MAZO");
 
-        UI.mostrarCartes(mazo1.getCartes());
-
+        //Donar les cartes al jugador //En teoria s'ha de canviar perquè li doni una per una
         for (int i = 0; i <= 6; i++){
             for (Jugador j : llistaJugador){
                 j.addCarta((mazo1.agafarCarta()));
             }
         }
-
-        System.out.println("Jugador 1 mà");
-        UI.mostrarCartes(j1.getCartes());
-
     }
-
 }

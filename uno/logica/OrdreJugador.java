bbe03ca8ca50198751e809d8cfd52ad1afbb9102
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-29 09:24:01 +0200

    Update: OrdreJugador(), crear Constructor OrdreJugador, crearJugador, barrejarOrdre, getJugadorActiu

--- uno/logica/OrdreJugador.java
+++ uno/logica/OrdreJugador.java
@@ -2,21 +2,37 @@ package uno.logica;
 
 import uno.interficie.UI;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class OrdreJugador {
-    public void passarTorn(){
+    private int jugadorActiu;
+    private ArrayList<Jugador> jugadors;
+    private ArrayList<String> nomsJugadors = UI.introduirJugadors();
 
+    public OrdreJugador(){
+        jugadors = new ArrayList<>();
+        this.jugadorActiu = 0;
     }
 
-    public void getJugadorActiu(){
-
-    }
-
-    public void crearJugador(String nom){
-
+    public void crearJugador(){
+        for (int i = 0; i<nomsJugadors.size(); i++){
+            String nom = nomsJugadors.get(i);
+            Jugador nouJugador = new Jugador(nom);
+        }
     }
 
     public void barrajarOrdre(){
-
+       Collections.shuffle(nomsJugadors);
     }
 
+    public String getJugadorActiu(){
+        return nomsJugadors.get(jugadorActiu);
+    }
+
+    public void passarTorn(){
+
+    }
 }

c5d463f820f12b15a3f184146b6e64d43ec96230
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-29 09:20:33 +0200

    Create: Regles.java i condicions

--- uno/logica/Regles.java
+++ uno/logica/Regles.java
@@ -1,7 +1,11 @@
 package uno.logica;
 
 public class Regles {
-    public boolean sonCompatibles(){
-        return false;
+    public boolean sonCompatibles(Carta carta, Carta carta2){
+        boolean colorsIguals = carta.getColor() == carta2.getColor();
+        boolean numsIguals = carta.getNumero() == carta2.getNumero();
+
+        return colorsIguals || numsIguals;
+
     }
 }

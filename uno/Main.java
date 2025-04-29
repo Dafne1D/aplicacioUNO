b4e06216d05d670aae7ccdcc8304c0c4f6856cc8
Dafne <dcarrillo572@boscdelacoma.cat>
2025-04-29 09:20:03 +0200

    Update: Jugar();

--- uno/Main.java
+++ uno/Main.java
@@ -1,39 +1,8 @@
 
 package uno;
 
-import uno.interficie.*;
-import uno.logica.*;
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-
-        //JUGADORS----------------------------
-        ArrayList<Jugador> llistaJugador = new ArrayList<Jugador>();
-
-        UI.introduirJugadors();
-
-        //MAZO-----------------------------
-        Mazo mazo1 = new Mazo();
-        mazo1.barrejar();
-        System.out.println("MAZO");
-        UI.mostrarCartes(mazo1.getCartes());
-
-        //PILO--------------------------------
-        Pilo pilo1 = new Pilo();
-        pilo1.getCartes();
-        for (int i = 0; i<1; i++){
-            pilo1.getCartes().push(mazo1.agafarCarta());
-        }
-        System.out.println("PILO");
-        UI.mostrarCartes(new ArrayList<>(pilo1.getCartes()));
-
-        //Donar les cartes al jugador //En teoria s'ha de canviar perquè li doni una per una
-        for (int i = 0; i <= 6; i++){
-            for (Jugador j : llistaJugador){
-                j.addCarta((mazo1.agafarCarta()));
-            }
-        }
+        Partida.Jugar();
     }
 }
